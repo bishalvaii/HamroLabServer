ec32bc8cbc9ac0ee1be6314ac07ac28081c3700a
Vishal Adhikari <vishaladhikari1738@gmail.com>
2023-04-09 16:52:46 +0545

    lab test api aded

--- server.js
+++ server.js
@@ -29,11 +29,15 @@ app.use(bodyParser.json());
 app.use(cors({ origin: 'http://localhost:3000' , methods: ['POST'] }));
 
 app.post('/api/labtests', async (req, res) => {
-    const { client, orderedBy, referrer, selectedTest, sampleTaken, sampleTime, remarks } = req.body;
+    const { client_name, ordered_by, referrer, test_name, sample_taken, sample_time, remarks } = req.body ;
+    if (!sample_time) {
+      return res.status(400).json({ message: 'date_time field is required' });
+    }
       console.log(req.body)
     try {
-      const result = await pool.query('INSERT INTO lab_tests(client, ordered_by, referrer, selectedTest, sample_taken, sample_time, remarks) VALUES($1, $2, $3, $4, $5, $6, $7)', [client, orderedBy, referrer, selectedTest, sampleTaken, sampleTime, remarks]);
-      res.status(200).json({ result: result.rows[0], message: 'Lab test data saved successfully' });
+      const client = await pool.connect();
+      const result = await client.query('INSERT INTO lab_tests(client_name, ordered_by, referrer, test_name, sample_taken, sample_time, remarks) VALUES($1, $2, $3, $4, $5, $6, $7 )RETURNING *', [client_name, ordered_by, referrer, test_name,  sample_taken, sample_time, remarks]);
+      res.status(200).json( result.rows[0] );
     
     } catch (err) {
       console.error(err);
@@ -45,18 +49,20 @@ app.post('/api/labtests', async (req, res) => {
   app.get('/api/labtests', async (req, res) => {
     try {
       const result = await pool.query('SELECT * FROM lab_tests');
-       res.status(200).json({ data: result.rows });
+       res.status(200).json({ data: result.rows  });
+       
     
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: 'Error fetching lab test data' });
     }
   });
+
   app.post("/api/create/patients", async (req, res) => {
   const { patient_name, disease_name, date_time, doctor_name, status } = req.body;
   console.log(req.body)
 
-  try {
+  try { 
     const client = await pool.connect();
 
     const result = await client.query(
@@ -66,16 +72,46 @@ app.post('/api/labtests', async (req, res) => {
     );
 
     res.status(201).json(result.rows[0]);
-  } catch (error) {
+  } catch (error) {   
     console.error(error);
     res.status(500).send("Internal server error");
   }
 });
+
+app.post("/api/create/doctor", async (req,res) => {
+  const {doctor_name, title, department, gender, email , phone, qualification} = req.body;
+
+  console.log(req.body)
+
+  try {
+    const client = await pool.connect();
+    const result = await client.query(
+      "INSERT INTO doctors (doctor_name, title, department, gender, email, phone, qualification) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [doctor_name, title, department, gender, email, phone, qualification] 
+    );
+    res.status(201).json(result.rows[0]);
+  }
+  catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server error")
+  }
+})
+app.get("/api/doctors", async (req,res) => {
+  try {
+    const client = await pool.connect();
+    const doctors = await client.query('SELECT * FROM doctors');
+    res.json(doctors.rows)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to retireve doctors"})
+  }
+})
 app.get("/api/patients", async (req, res) => {
   try {
     const client = await pool.connect();
 
     const patients = await client.query("SELECT * FROM patients");
+    console.log(patients)
     res.json(patients.rows);
   } catch (error) {
     console.error(error);
